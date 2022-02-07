1b66c8434f71475daa2249bc51c24dbf92119918
Joe Roe <git@joeroe.io>
2022-02-07 16:38:15 +0100

    Fix call to mockery::stub

--- tests/testthat/test-paleoclim.R
+++ tests/testthat/test-paleoclim.R
@@ -4,12 +4,12 @@ test_that('paleoclim files can be read as rasters', {
 })
 
 test_that('paleoclim() returns a raster without error', {
-  mockery::stub(paleoclim, "curl_download", mock_download)
+  mockery::stub(paleoclim, "curl::curl_download", mock_download)
   expect_error(pc <- paleoclim("lh", "10m"), NA)
   expect_s4_class(pc, "Raster")
 })
 
 test_that('quiet means quiet', {
-  mockery::stub(paleoclim, "curl_download", mock_download)
+  mockery::stub(paleoclim, "curl::curl_download", mock_download)
   expect_silent(paleoclim("lh", "10m", quiet = TRUE))
 })
